ba8fda6694e7d5e289a8c1dd5f424d7360148eac
Atharva <atharva.deshmukh@siemens.com>
2024-03-19 20:21:10 +0530

    chore: container did run successfully

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.get('/', (request, response) => {
 });
 
 // listening this app at port 5000
-app.listen(5000, () => {
-    console.log('App is running on port 5000')
+app.listen(5500, () => {
+    console.log('App is running on port 5500')
 });
